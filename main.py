2c24de43545f6a253b22d7a5d9622462b3f5bbeb
ahmadaizoki <aizoki.ahmad@gmail.com>
2017-07-13 15:43:03 +0200

    bot

--- main.py
+++ main.py
@@ -34,25 +34,25 @@ def webhook():
             sender = data['conversation']['id']
             if 'membersRemoved' in data.keys():
                 left_member = data['recipient']['name']
-                
-                print "I got removed from a group"
-                
+
+                print ("I got removed from a group")
+
             elif 'membersAdded' in data.keys():
                 new_member = data['recipient']['name']
-                
+
                 bot.send_message(service,sender,"Hi, I am emoji bot. I can transform your text in messages to emojies. I also have an emoji game to play simply send @emojirobor #emojigame")
             else:
                 pass
         elif data['type'] =='message':
-            
+
             if 'isGroup' in data['conversation'].keys():
                 sender = data['conversation']['id']
                 text = data['text']
-                
+
                 #do what ever you want to do here for GROUPS
-                    
+
                 process_messages(sender,text,service)
-                    
+
             else:
                 #private chat
                 sender = data['conversation']['id']
@@ -62,9 +62,9 @@ def webhook():
 
         elif data['type'] == 'contactRelationUpdate':
         #bot added for private chat
-        
+
             if data['action']=='add':
-            
+
                 sender = data['conversation']['id']
                 bot.send_message(service,sender,"Hi, I am a bot.")
                 pass
@@ -72,7 +72,7 @@ def webhook():
                 pass
             else:
                 pass
-                
+
         else:
             pass
     except Exception as e:
@@ -84,10 +84,10 @@ def webhook():
 
 def process_messages(sender,text,service):
   bot.send_message(service,sender,"Hello World")
-        
-        
-        
-        
+
+
+
+
 if __name__ == '__main__':
     #context = ('/etc/ssl/localcerts/mycert.pem', '/etc/ssl/localcerts/mykey.key')
     #app.run(host='127.0.0.1',port=8000,debug=False,ssl_context=context)
