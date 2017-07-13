7d78f4e0615228100ab2d4a7fc6ee818d81e45c3
ahmadaizoki <aizoki.ahmad@gmail.com>
2017-07-13 15:37:38 +0200

    bot

--- skypebot/__init__.py
+++ skypebot/__init__.py
@@ -4,16 +4,19 @@ import threading
 
 import sys
 import requests
+import time
+from datetime import *
+from time import *
 
 from skypebot import skype_api
 
 
 
 class SkypeBot:
-    
+
     def __init__(self, client_id,client_secret):
-        
-             
+
+
         def token_func():
             global token
             payload = "grant_type=client_credentials&client_id="+client_id+"&client_secret="+client_secret+"&scope=https%3A%2F%2Fgraph.microsoft.com%2F.default"
@@ -25,36 +28,36 @@ class SkypeBot:
         def runit():
             while True:
                 token_func()
-                time.sleep(3000)
+                sleep(3000)
 
-        self.t = threading.Thread(target=runtit)
+        self.t = threading.Thread(target=runit)
         self.t.daemon = True
         self.t.start()
-        
+
     def send_message(self,service,sender, text):
 
         return skype_api.send_message(token,service,sender, text)
 
     def create_card_image(self,url,alt=None):
         return skype_api.create_card_image(url,alt)
-        
+
     def create_buttons(self,type,title,value):
         return skype_api.create_buttons(type,title,value)
-        
+
     def create_card_attachment(self,type,title,subtitle=None,text=None,images=None,buttons=None):
         return skype_api.create_card_attachment(type,title,subtitle,text,images,buttons)
 
     def create_animation(self,type,url,images,title=None,subtitle=None,text=None,buttons=None):
-    return skype_api.create_animation(type,url,images,title,subtitle,text,buttons)
-    
+        return skype_api.create_animation(type,url,images,title,subtitle,text,buttons)
+
 
     def send_media(self,service,sender,type,url):
         return skype_api.send_media(token,service,sender,type,url)
-        
+
     def send_card(self,service,sender,type,card_attachment,summary=None,text=None):
         return skype_api.send_card(token,service, sender,type,card_attachment,summary,text)
-        
+
     #Not yet supported
-    
+
     def send_action(self,service,sender):
-        return skype_api.send_action(token,service,sender) 
+        return skype_api.send_action(token,service,sender)
--- skypebot/skype_api.py
+++ skypebot/skype_api.py
@@ -32,18 +32,18 @@ def send_message(token,service,sender,text):
     except Exception as e:
         print e
         pass
-        
 
-        
-#openUrl  URL to be opened in the built-in browser.  
-#imBack  Text of message which client will sent back to bot as ordinary chat message. All other participants will see that was posted to the bot and who posted this.  
-#postBack  Text of message which client will post to bot. Client applications will not display this message.  
-#call  Destination for a call in following format: "tel:123123123123"  
-#playAudio  playback audio container referenced by URL  
-#playVideo  playback video container referenced by URL  
-#showImage  show image referenced by URL  
-#downloadFile  download file referenced by URL  
-#signin  OAuth flow URL  
+
+
+#openUrl  URL to be opened in the built-in browser.
+#imBack  Text of message which client will sent back to bot as ordinary chat message. All other participants will see that was posted to the bot and who posted this.
+#postBack  Text of message which client will post to bot. Client applications will not display this message.
+#call  Destination for a call in following format: "tel:123123123123"
+#playAudio  playback audio container referenced by URL
+#playVideo  playback video container referenced by URL
+#showImage  show image referenced by URL
+#downloadFile  download file referenced by URL
+#signin  OAuth flow URL
 
 
 def create_buttons(type,title,value):
@@ -53,16 +53,16 @@ def create_buttons(type,title,value):
 	buttons_dict["title"] = title
 	#buttons_dict['image'] = image
 	buttons_dict["value"] = value
-	
+
 	return buttons_dict
-	
-	
+
+
 def create_card_image(url,alt):
 	img_dict = {}
 	img_dict["url"] = url
 	img_dict["alt"] = alt
 	return img_dict
-	
+
 def create_card_attachment(type,title,subtitle,text,images,buttons):
     card_attachment={}
     card_attachment={
@@ -75,7 +75,7 @@ def create_card_attachment(type,title,subtitle,text,images,buttons):
 			"buttons": buttons
             }
         }
-    
+
     return card_attachment
 
 
@@ -83,43 +83,43 @@ def create_card_attachment(type,title,subtitle,text,images,buttons):
 def send_media(token,service,sender,type,url):
     try:
         response = requests.get(url).content
-        
+
         payload = {
-            "type":"message",  
+            "type":"message",
             "attachments": [{
                 "contentType":type,
                 "contentUrl": url
             }]
         }
-        
+
         r = requests.post(service+'/v3/conversations/'+sender+'/activities/',headers={"Authorization": "Bearer "+token,"Content-Type":"application/json"},json=payload)
         print r
     except Exception as e:
         print e
         pass
-        
-        
-	
-	
+
+
+
+
 def send_card(token,service,sender,type,card_attachment,summary,text):
     try:
         payload = {
-        "type":"message,
+        "type":message,
         "attachmentLayout":type,
         "summary":summary,
         "text":text,
         "attachments":card_attachment
         }
-        
+
         r = requests.post(service+'/v3/conversations/'+sender+'/activities/',headers={"Authorization": "Bearer "+token,"Content-Type":"application/json"},json=payload)
         print payload
         print r
     except Exception as e:
         print e
         pass
-        
+
 #typing action not yet supported
-        
+
 def send_action(token,service,sender):
     try:
         payload = {
@@ -149,5 +149,5 @@ def create_animation(type,url,images,title,subtitle,text,buttons):
             "buttons": buttons
             }
         }
-    
+
     return card_animation
