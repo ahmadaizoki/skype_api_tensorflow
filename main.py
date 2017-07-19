8fb43b8c344fc4cb28d7c356d81a2d9f2f81c0ce
ahmadaizoki <aizoki.ahmad@gmail.com>
2017-07-19 16:08:03 +0200

    skype

--- main.py
+++
@@ -1,8 +0,0 @@
-import Tensorflow_chat_bot_response as bt
-
-def main():
-    print bt.response('c est quoi les horaires du petit-dej')
-    print bt.response('on peut se baigner jusqu a quelle heure')
-
-if __name__=='__main__':
-    main()
